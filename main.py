6213ea8dd4d2dc7f106b23c4de9c97dd21653d84
sn00wden <matthieu2603@gmail.com>
2022-11-23 17:08:55 +0100

    GET and POST done

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from fastapi import FastAPI
 
+
+todos = []
+class Task:
+    def __init__(self, title: str, description: str):
+        self.id = len(todos)
+        self.title = title
+        self.state = 0 # 0: todo, 1: done
+        self.due_date = "2021-12-31"
+        self.description = description
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "state": self.state,
+            "due_date": self.due_date,
+            "description": self.description
+        }
+        
+
 app = FastAPI()
 
 
@@ -14,19 +34,25 @@ async def gregor():
 #Todo example
 @app.get("/todos")
 async def get_todos():
-    return [{"title": "Foo"}, {"title": "Bar"}]
+    data_to_dict = [todo.to_dict() for todo in todos]
+    return data_to_dict
 
 @app.get("/todos/{todo_id}")
 async def get_todo(todo_id: int):
-    return {"title": "Foo"}
+    for task in todos:
+        if task.id == todo_id:
+            return task.to_dict()
+    return {"message": "Todo not found"}
 
 @app.post("/todos")
 async def create_todo():
-    return {"title": "Foo"}
+    todo = Task("Foo", "Bar")
+    todos.append(todo)
+    return {"message": "Todo created"}
 
-@app.put("/todos/{todo_id}")
-async def update_todo(todo_id: int):
-    return {"title": "Foo"}
+# @app.put("/todos/{todo_id}")
+# async def update_todo(todo_id: int):
+#     return {"title": "Foo"}
 
 @app.delete("/todos/{todo_id}")
 async def delete_todo(todo_id: int):
